d97b1ccdf90e36692df5aa0ca86f80073c944472
Yonathan Wondosen <112022096+yonathan001@users.noreply.github.com>
2025-05-25 17:29:38 +0300

    any

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,7 @@ let sales: number =123_456_789;
 let course: string = "TypeScript Fundamentals";
 let isPublished: boolean = true;
 let level;
+
+function render(document: any) {
+    console.log(document);
+}
